499927482bbee84ec52e792f0bc08184a839b254
NitinParamkar <nitinparamkar@gmail.com>
2024-06-11 00:15:00 +0530

    added initial files

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Hero.css'
+import Header from '../Header/Header'
+const Hero = () => {
+  return (
+    <div className="hero">
+         <div className="left-h">
+            <Header/>
+            <div className="the-best-ad">
+                <div></div>
+                <span>the best fitness club in the city</span>
+            </div>
+         </div>
+         <div className="right-h">right side</div>
+    </div>
+  )
+}
+
+export default Hero
